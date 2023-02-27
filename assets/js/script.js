11cc8919456ddc436868d3904db0854cff5b913c
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-02-26 23:30:56 -0500

    wrap all code in .ready() function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,5 @@
 /**
  * TODO: Add code to display the current date in the header of the page.
- * TODO: Add a listener for click events on the save button - use 'this' in DOM traversal
- * TODO: Add code to get any user input that was saved in localStorage and set value of corresponding textarea element
- * TODO: Wrap all code that interacts with the DOM in a call to jQuery
  * 
  * --- EXTRAS ----
  * TODO: refresh time at top of hour using setTimeout
@@ -12,64 +9,70 @@
  -----------------------------------------------------------------------
  */
 
-/* -------------------------------------------------------------------------------------------
+$(function () {
+  /* ---------------------------------------------------------------------
 TIME BLOCKS
 */
-// Create time block HTML and append to <main>
-function createTimeBlock(time) {
-  var timeBlock = $('<div class="time-block row">').attr("id", "hour-" + time);
-  var hour = $('<div class="col-2 col-md-1 text-center py-3 hour">');
-  var textarea = $(
-    '<textarea class="col-8 col-md-10 description" rows="3"> </textarea>'
-  ).val(localStorage.getItem("hour-" + time));
-  var button = $(
-    '<button class="btn saveBtn col-2 col-md-1" aria-label="save"><i class="fas fa-save" aria-hidden="true"></i></button>'
-  );
-  $(timeBlock).append(hour, textarea, button);
-  $("main").append(timeBlock);
-}
-
-// Add time data to time block and compare to current time
-function addTimeData(time, IdSelector) {
-  var setTime = dayjs().hour(time);
-  var currentHour = now.hour();
-  var setHour = setTime.hour();
-  $(IdSelector).children(".hour").text(setTime.minute(0).format("H:mm A"));
-  // Compare and apply relevant class (past, future, or present)
-  if (setHour < currentHour) {
-    $(IdSelector).addClass("past");
-  } else if (setHour == currentHour) {
-    $(IdSelector).addClass("present");
-  } else if (setHour > currentHour) {
-    $(IdSelector).addClass("future");
+  // Create time block HTML, pull in any existing data, and append to <main>
+  function createTimeBlock(time) {
+    var timeBlock = $('<div class="time-block row">').attr(
+      "id",
+      "hour-" + time
+    );
+    var hour = $('<div class="col-2 col-md-1 text-center py-3 hour">');
+    var textarea = $(
+      '<textarea class="col-8 col-md-10 description" rows="3"> </textarea>'
+    ).val(localStorage.getItem("hour-" + time));
+    var button = $(
+      '<button class="btn saveBtn col-2 col-md-1" aria-label="save"><i class="fas fa-save" aria-hidden="true"></i></button>'
+    );
+    $(timeBlock).append(hour, textarea, button);
+    $("main").append(timeBlock);
   }
-}
 
-// Generate all time blocks and add time data
-function generateTimeBlocks() {
-  for (let i = firstHour; i <= lastHour; i++) {
-    var timeBlockEl = "#hour-" + i;
-    createTimeBlock(i);
-    addTimeData(i, timeBlockEl);
+  // Add time data to time block and compare to current time
+  function addTimeData(time, IdSelector) {
+    var setTime = dayjs().hour(time);
+    var currentHour = now.hour();
+    var setHour = setTime.hour();
+    $(IdSelector).children(".hour").text(setTime.minute(0).format("H:mm A"));
+    // Compare and apply relevant class (past, future, or present)
+    if (setHour < currentHour) {
+      $(IdSelector).addClass("past");
+    } else if (setHour == currentHour) {
+      $(IdSelector).addClass("present");
+    } else if (setHour > currentHour) {
+      $(IdSelector).addClass("future");
+    }
   }
-}
 
-/* -------------------------------------------------------------------------------------------
+  // Generate all time blocks and add time data
+  function generateTimeBlocks() {
+    for (let i = firstHour; i <= lastHour; i++) {
+      var timeBlockEl = "#hour-" + i;
+      createTimeBlock(i);
+      addTimeData(i, timeBlockEl);
+    }
+  }
+
+  /* ---------------------------------------------------------------------
 TEXT AREA
 */
-// store textarea value in local storage with parent id as key
-function storeText(event) {
-  var parentId = $(event.currentTarget).parent().attr("id");
-  var textareaValue = $(event.currentTarget).prev().val();
-  localStorage.setItem(parentId, textareaValue);
-}
+  // store textarea value in local storage with parent id as key
+  function storeText(event) {
+    var parentId = $(event.currentTarget).parent().attr("id");
+    var textareaValue = $(event.currentTarget).prev().val();
+    localStorage.setItem(parentId, textareaValue);
+  }
 
-// run storeText on button click
-$("main").on("click", ".saveBtn", storeText);
+  // run storeText on button click
+  $("main").on("click", ".saveBtn", storeText);
 
-// -------------------------------------------
-
-var now = dayjs();
-var firstHour = 17;
-var lastHour = 23;
-generateTimeBlocks();
+  /* ---------------------------------------------------------------------
+RUN FUNCTIONS
+*/
+  var now = dayjs();
+  var firstHour = 17;
+  var lastHour = 23;
+  generateTimeBlocks();
+});
